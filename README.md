81b0f279f0a4482d6df1d9eff13a814de0a2ba90
Kevin Hrpcek <khrpcek@gmail.com>
2016-01-18 14:28:40 -0600

    New version. See readme

--- README.md
+++ README.md
@@ -10,22 +10,46 @@ This is a small script for doing mass SSH operations on servers. There are curre
 
 Use pip to install these or do it manually.
 
+###Usage
+usage: sshdov2.py [-h] [--put PUT] [--get GET] [--debug]
+
+Run a command or sftp on many servers. Only sshdo operations are logged, put
+and get are not logged since there is generally no output. Running without put
+or get allows for ssh operation.
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --put PUT   Use sftp to put files
+  --get GET   Use sftp to get files. Files are prefixed with the hostname they
+              are from.
+  --debug     Enables paramiko debug logging. Goes to logs/debug.log
+
+
 ###sshdo
 Usage: ./sshdo.py
 Executes the given command on all hosts in the list or typed in.
 
 ###put
-Usage: ./sshdo.py put
+Usage: ./sshdo.py --put
 This sftp's the given file to the given location on all remote hosts in the list or typed in manually.
 
 ###get
-Usage: ./sshdo get
+Usage: ./sshdo --get
 SFTPs the given file from each server to the given location on the host you are running sshdo on. It will prefix the file name with the host/ip you connected to the remote host with.
+
+###Changelog
+* 1/18/2016
+** Cleaned up code. Reduced all code to 1 file. 
+** Removed base64 module
+** It now requires shutil, argparse, and tarfile modules
+** Fixed Logging
+** Added log compression. It tar/gzips the log folders older than 4 operations. You can change this value by setting the untar variable in the script.
+** Prints list at end of script for failed hosts
+** Prints log directory for current operation at end of script
+** Now uses full paths for files so that you can execute sshdo from outside it's own directory
+
 
 ###TODO
 * More exception handling
 * Option to disable logging
-* Log Compression
-* Log rotation
 * Threading option
-* Failed list at end instead of in standard out
--- sshdo.py
+++ sshdo.py
@@ -1,27 +1,177 @@
 #!/usr/bin/python
 
 #sshdo, get, cp, all compiled into one script
-#add in logging capabilities
 #use list files in a subdirectory for sources
 #but have it just read in ./sshdo.py asdf and it reads asdf.txt
 # run as ./sshdo list cp,get
 
-import sys
-import subprocess
-import paramiko
-import base64
-from sshdef import *
+import sys, os, time, argparse
+import paramiko, tarfile, shutil
+from colors import red, green
 
-if len(sys.argv) > 1:
+def agent_auth(transport, username):
+    """
+    Attempt to authenticate to the given transport using any of the private
+    keys available from an SSH agent.
+    """
+    agent = paramiko.Agent()
+    agent_keys = agent.get_keys()
+    if len(agent_keys) == 0:
+        return
+    for key in agent_keys:
+        print('Trying ssh-agent key %s' % hexlify(key.get_fingerprint()))
+        try:
+            transport.auth_publickey(username, key)
+            print('... success!')
+            return
+        except paramiko.SSHException:
+            print('... nope.')
 
-    if sys.argv[1] == "get":
-        sshget()
-    elif sys.argv[1] == "put":
+def explode_list(list_name):
+    file_name = 'lists/' + list_name
+    servers_file = open(file_name, 'r')
+    servers_list = servers_file.readlines()
+    servers_list_stripped = [line.rstrip() for line in servers_list]
+    servers_file.close()
+    return servers_list_stripped
+
+def server_list(server_input):
+    if len(server_input.split()) > 1:
+        list = server_input.split()
+    elif len(server_input.split()) == 1:
+        list = explode_list(server_input)
+    return list
+
+def sshdo(args, sshdo_dir):
+    servers = raw_input('Servers: ')
+    command = raw_input('Command: ')
+    #if not os.path.exists('logs'+datetime): os.makedirs('logs/'+datetime)
+    if not os.path.exists("{0}/logs/{1}".format(sshdo_dir, log_dir)): os.makedirs("{0}/logs/{1}".format(sshdo_dir, log_dir))
+
+    list = server_list(servers)
+
+    fail_to_connect = []
+    for host in list:
+        try:
+            ssh.connect(host, username='root', allow_agent=True, timeout=5)
+        except Exception:
+            print red(host + "\n Could not connect \n")
+            fail_to_connect.append(host)
+        else:
+#            log_f = open('logs/'+datetime+'/'+host+'.log', 'w')
+            log_f = open("{0}/logs/{1}/{2}.log".format(sshdo_dir, log_dir, host), 'w')
+            log_f.write(command+"/n/n")
+            if args.debug:
+                paramiko.util.log_to_file("{0}/logs/debug.log".format(sshdo_dir))
+            print green("\n" + host + "\n")
+            stdin, stdout, stderr = ssh.exec_command(command)
+            for data_line in stdout:
+                print data_line.rstrip()
+                log_f.write(data_line)
+            log_f.close()
+            ssh.close()
+    if len(fail_to_connect) > 0:
+        print red("Failed to connect to these hosts %s" % (fail_to_connect))
+    print "Log files are in logs/%s" % (log_dir)
+    return
+
+def sshput():
+    print "sshput block"
+
+    #prompt for info
+    servers = raw_input('Servers: ')
+    local_file = raw_input('Local File or Direcotry: ')
+    remote_dir = raw_input('Remote Location: ')
+    remote_base_file = os.path.basename(local_file)
+    remote_file = remote_dir + remote_base_file
+
+    list = server_list(servers)
+
+    fail_to_connect = []
+    for host in list:
+        try:
+            ssh.connect(host, username='root', allow_agent=True, timeout=5)
+        except Exception:
+            print red(host + "\n Could not connect \n")
+            fail_to_connect.append(host)
+        else:
+            print green("\n" + host + "\n")
+            sftp = ssh.open_sftp()
+            sftp.put(local_file,remote_file,confirm=True)
+            ssh.close()
+    if len(fail_to_connect) > 0:
+        print red("Failed to connect to these hosts %s" % (fail_to_connect))
+    return "sshput"
+
+def sshget():
+
+    #prompt for info
+    servers = raw_input('Servers: ')
+    remote_file = raw_input('Remote File: ')
+    local_dir = raw_input('Local Drop Directory: ')
+    remote_base_file = os.path.basename(remote_file)
+
+    list = server_list(servers)
+
+    fail_to_connect = []
+    for host in list:
+        try:
+            ssh.connect(host, username='root', allow_agent=True, timeout=5)
+        except Exception:
+            print red(host + "\n Could not connect \n")
+            fail_to_connect.append(host)
+        else:
+            print green("\n" + host + "\n")
+            local_file = local_dir + host + "." + remote_base_file
+            sftp = ssh.open_sftp()
+            sftp.get(remote_file,local_file)
+            ssh.close()
+    if len(fail_to_connect) > 0:
+        print red("Failed to connect to these hosts %s" % (fail_to_connect))
+    return "sshget"
+def logtar(sshdo_dir):
+#keep these many dirs untar'd
+    untar=4
+    logs_list = os.listdir('%s/logs' % (sshdo_dir))
+    tars = [s for s in logs_list if "tar.gz" in s]
+    for i in tars:
+        logs_list.remove(i)
+    if '.gitignore' in logs_list:
+        logs_list.remove('.gitignore')
+    if 'debug.log' in logs_list:
+        logs_list.remove('debug.log')
+#at this point logs_list has excluded .gitignore and all tar.gz files
+    if (len(logs_list) == 0) or (len(logs_list) <= untar):
+        exit()
+    elif len(logs_list) > untar:
+        #datetime_files = [datetime.strptime(a, "%m%d%Y-%H%M%S") for a in logs_list]
+        #sorted_list = [datetime.strftime(a, "%m%d%Y-%H%M%S") for a in datetime_files]
+        sorted_list = sorted(logs_list)
+        sorted_list = sorted_list[:len(sorted_list)-untar]
+        for i in sorted_list:
+            tar = tarfile.open("%s/logs/%s.tar.gz" % (sshdo_dir, i), "w:gz")
+            tar.add("%s/logs/%s" % (sshdo_dir, i), arcname=i)
+            tar.close()
+            shutil.rmtree("%s/logs/%s" % (sshdo_dir, i))
+    return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run a command or sftp on many servers. Only sshdo operations are logged, put and get are not logged since there is generally no output. Running without put or get allows for ssh operation.')
+    parser.add_argument('--put',help='Use sftp to put files')
+    parser.add_argument('--get',help='Use sftp to get files. Files are prefixed with the hostname they are from.')
+    #not built in yet, next version perhaps parser.add_argument('--nolog', help='Disables logging')
+    parser.add_argument('--debug', help='Enables paramiko debug logging. Goes to logs/debug.log',action='store_true')
+    args = parser.parse_args()
+    
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    log_dir = time.strftime("%m%d%Y-%H%M%S")
+    sshdo_dir = os.path.dirname(os.path.realpath(__file__))
+    
+    if args.put:
         sshput()
-    elif sys.argv[1] == "help":
-        print "help info for n00bz"
-elif len(sys.argv) == 1:
-    #run sshdo
-#    print sshdo_questions()
-    print sshdo()
-
+    elif args.get:
+        sshget()
+    else:
+        sshdo(args, sshdo_dir)
+        logtar(sshdo_dir)
